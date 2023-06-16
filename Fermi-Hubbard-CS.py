55ea298085b72d12d931adc74d0920f96772620c
ozlemba <bayrakli.ozlemnur@metu.edu.tr>
2023-06-16 14:32:05 +0300

    fermi_ozlem

--- Fermi-Hubbard-CS.py
+++ Fermi-Hubbard-CS.py
@@ -84,8 +84,63 @@ print(res)
 
 
 
-hamiltonian_jw = JordanWignerMapper().map(ham).to_matrix()
+hamiltonian_jw = JordanWignerMapper().map(ham)
 #print(np.count_nonzero(hamiltonian_jw - np.diag(np.diagonal(hamiltonian_jw)))
+from qiskit import QuantumCircuit
+from qiskit.extensions import HamiltonianGate
+from qiskit.extensions import UnitaryGate
 print(hamiltonian_jw)
 
 
+from qiskit.algorithms import NumPyMinimumEigensolver
+from qiskit.opflow import PauliSumOp
+
+numpy_solver = NumPyMinimumEigensolver()
+result = numpy_solver.compute_minimum_eigenvalue(operator=PauliSumOp(hamiltonian_jw))
+ref_value = result.eigenvalue.real
+print(f"Reference value: {ref_value:.5f}")
+from qiskit.circuit.library import TwoLocal
+from qiskit.algorithms.optimizers import SPSA
+
+iterations = 125
+ansatz = TwoLocal(rotation_blocks="ry", entanglement_blocks="cz")
+spsa = SPSA(maxiter=iterations)
+counts = []
+values = []
+
+
+def store_intermediate_result(eval_count, parameters, mean, std):
+    counts.append(eval_count)
+    values.append(mean)
+from qiskit.utils import algorithm_globals
+from qiskit_aer.primitives import Estimator as AerEstimator
+
+seed = 170
+algorithm_globals.random_seed = seed
+
+noiseless_estimator = AerEstimator(
+    run_options={"seed": seed, "shots": 1024},
+    transpile_options={"seed_transpiler": seed},
+)
+
+from qiskit.algorithms.minimum_eigensolvers import VQE
+
+vqe = VQE(
+    noiseless_estimator, ansatz, optimizer=spsa, callback=store_intermediate_result
+)
+result = vqe.compute_minimum_eigenvalue(operator=hamiltonian_jw)
+
+print(f"VQE on Aer qasm simulator (no noise): {result.eigenvalue.real:.5f}")
+print(
+    f"Delta from reference energy value is {(result.eigenvalue.real - ref_value):.5f}"
+)
+import matplotlib.pyplot as plt
+
+
+plt.plot(counts, values)
+plt.xlabel("Eval count")
+plt.ylabel("Energy")
+plt.title("Convergence with no noise")
+
+plt.show()
+print(f'Number of qubits: {hamiltonian_jw.num_qubits}')
