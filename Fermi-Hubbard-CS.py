d46b34be430db28255278591d0fb172f9d1ae22f
ozlemba <bayrakli.ozlemnur@metu.edu.tr>
2023-06-16 15:03:19 +0300

    fermi_ozlem

--- Fermi-Hubbard-CS.py
+++ Fermi-Hubbard-CS.py
@@ -13,7 +13,14 @@ from qiskit_nature.second_q.hamiltonians.lattices import (
     TriangularLattice,
 )
 from qiskit_nature.second_q.hamiltonians import FermiHubbardModel
-square_lattice = SquareLattice(rows=5, cols=4, boundary_condition=BoundaryCondition.PERIODIC)
+square_lattice = SquareLattice(rows=3, cols=2, boundary_condition=BoundaryCondition.PERIODIC)
+size = (2, 2, 2)
+boundary_condition = (
+    BoundaryCondition.PERIODIC,
+    BoundaryCondition.PERIODIC,
+    BoundaryCondition.PERIODIC,
+)
+cubic_lattice = HyperCubicLattice(size=size, boundary_condition=boundary_condition)
 
 from qiskit_nature.settings import QiskitNatureSettings
 QiskitNatureSettings.use_pauli_sum_op = False
@@ -23,47 +30,17 @@ v = 0.0  # the onsite potential
 u = 5.0  # the interaction parameter U
 
 fhm = FermiHubbardModel(
-    square_lattice.uniform_parameters(
+    cubic_lattice.uniform_parameters(
         uniform_interaction=t,
         uniform_onsite_potential=v,
     ),
     onsite_interaction=u,
 )
-ham = fhm.second_q_op().simplify()
-print(ham)
-graph = rx.PyGraph(multigraph=False)  # multiigraph shoud be False
-graph.add_nodes_from(range(6))
-weighted_edge_list = [
-    (0, 1, 1.0 + 1.0j),
-    (0, 2, -1.0),
-    (2, 3, 2.0),
-    (4, 2, -1.0 + 2.0j),
-    (4, 4, 3.0),
-    (2, 5, -1.0),
-]
-graph.add_edges_from(weighted_edge_list)
-
-general_lattice = Lattice(graph)  # the lattice whose weights are seen as the interaction matrix.
-u = 5.0  # the interaction parameter U
-
-fhm = FermiHubbardModel(lattice=general_lattice, onsite_interaction=u)
 
 ham = fhm.second_q_op().simplify()
 print(ham)
 
 from qiskit_nature.second_q.problems import LatticeModelProblem
-
-num_nodes = 4
-boundary_condition = BoundaryCondition.OPEN
-line_lattice = LineLattice(num_nodes=num_nodes, boundary_condition=boundary_condition)
-
-fhm = FermiHubbardModel(
-    line_lattice.uniform_parameters(
-        uniform_interaction=t,
-        uniform_onsite_potential=v,
-    ),
-    onsite_interaction=u,
-)
 
 lmp = LatticeModelProblem(fhm)
 from qiskit.algorithms.minimum_eigensolvers import NumPyMinimumEigensolver
