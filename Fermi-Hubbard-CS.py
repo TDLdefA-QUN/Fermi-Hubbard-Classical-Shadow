50a4899ad725cc01b4e1c460ef32c92af2568435
obm <mbaris@metu.edu.tr>
2023-06-20 16:44:06 +0300

    Classical shadow with pennylane

--- Fermi-Hubbard-CS.py
+++ Fermi-Hubbard-CS.py
@@ -14,12 +14,16 @@ from qiskit.algorithms.optimizers import SPSA
 from qiskit.utils import algorithm_globals
 from qiskit_aer.primitives import Estimator as AerEstimator
 from qiskit.algorithms.minimum_eigensolvers import VQE
-
+from qiskit.algorithms.optimizers import Optimizer, OptimizerResult, OptimizerState
+import psutil
 
 t = -1.0  # the interaction parameter
 v = 0.0  # the onsite potential
 u = 5.0
 
+###
+### Second Quantized Hamiltonian (Fermi-Hubbard model)
+###
 square_lattice = SquareLattice(rows=3, cols=2, boundary_condition=BoundaryCondition.PERIODIC)
 
 fhm_square = FermiHubbardModel(
@@ -30,9 +34,6 @@ fhm_square = FermiHubbardModel(
     onsite_interaction=u,
 )
 
-
-
-
 size = (2, 2, 2)
 boundary_condition = (
     BoundaryCondition.PERIODIC,
@@ -41,37 +42,43 @@ boundary_condition = (
 )
 cubic_lattice = HyperCubicLattice(size=size, boundary_condition=boundary_condition)
 
-
 QiskitNatureSettings.use_pauli_sum_op = False
-
-t = -1.0  # the interaction parameter
-v = 0.0  # the onsite potential
-u = 5.0  # the interaction parameter U
 
 fhm_cubic = FermiHubbardModel(
     cubic_lattice.uniform_parameters(
         uniform_interaction=t,
-        uniform_onsite_potential=v, 
+        uniform_onsite_potential=v,
     ),
     onsite_interaction=u,
 )
 
 ham = fhm_cubic.second_q_op()
 print(ham)
+###
 
 
+###
+### Embed the Hamiltonian
+###
 
 lmp = LatticeModelProblem(fhm_cubic)
 
 hamiltonian_jw = JordanWignerMapper().map(ham)
-#print(np.count_nonzero(hamiltonian_jw - np.diag(np.diagonal(hamiltonian_jw)))
+# print(np.count_nonzero(hamiltonian_jw - np.diag(np.diagonal(hamiltonian_jw)))
 print(hamiltonian_jw)
 
-
+###
+### Classical solver
+###
 numpy_solver = NumPyMinimumEigensolver()
 result = numpy_solver.compute_minimum_eigenvalue(operator=PauliSumOp(hamiltonian_jw))
 ref_value = result.eigenvalue.real
 print(f"Reference value: {ref_value:.5f}")
+
+###
+### VQE See Peruzzo, A., et al, “A variational eigenvalue solver on a quantum processor” arXiv:1304.3061
+###
+
 
 iterations = 125
 ansatz = TwoLocal(rotation_blocks="ry", entanglement_blocks="cz")
@@ -91,12 +98,9 @@ algorithm_globals.random_seed = seed
 noiseless_estimator = AerEstimator(
     run_options={"seed": seed, "shots": 1024},
     transpile_options={"seed_transpiler": seed},
-    backend_options={"method":"density_matrix"}
+    backend_options={"method": "automatic"}
 
 )
-
-
-
 
 vqe = VQE(
     noiseless_estimator, ansatz, optimizer=spsa, callback=store_intermediate_result
@@ -109,13 +113,20 @@ print(
 )
 import matplotlib.pyplot as plt
 
-
 plt.plot(counts, values)
 plt.xlabel("Eval count")
 plt.ylabel("Energy")
 plt.title("Convergence with no noise")
 
 plt.show()
+
+##
+## Reconstruct the state
+##
+result.optimal_circuit.draw(output='mpl')
+
+
 print(f'Number of qubits: {hamiltonian_jw.num_qubits}')
-import psutil
-print('RAM Used (GB):', psutil.virtual_memory()[3]/1000000000)
+
+print('RAM Used (GB):', psutil.virtual_memory()[3] / 1000000000)
+
